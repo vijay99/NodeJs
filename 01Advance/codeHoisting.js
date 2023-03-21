6e16acf6ac1264661538135687e80883b690c641
Kumar Vijay <vijay81238@gmail.com>
2023-03-21 17:03:53 +0530

    updated code hoisting comment

--- 01Advance/codeHoisting.js
+++ 01Advance/codeHoisting.js
@@ -1,3 +1,9 @@
+//COde hoisting means we should not be using a variable and declare it later on ,we should
+ //first be declaring it and using it or may be just on go declare it at the same time intialize 
+ //it as well.
+
+
+
 var getData = (temp = cold,cold=10) => {
     console.log(temp);
     console.log(cold);
